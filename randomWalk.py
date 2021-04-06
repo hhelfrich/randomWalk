afef48bd988cf5e463afe9d6f369e043bb0dbe93
Halle Helfrich <hallehelfrich@dhcp-172-16-243-61.dyn.pittstate.edu>
2021-04-06 11:07:16 -0500

    year step update

--- randomWalk.py
+++ randomWalk.py
@@ -3,27 +3,33 @@ import math
 import random
 import matplotlib.pyplot as plt
 
-def step(pos, N, mu, sigma):
+def step(pos, mu, sigma, N):
     dx = np.random.normal(mu, sigma, N)
     dy = np.random.normal(mu, sigma, N)
     dz = np.random.normal(mu, sigma, N)
     pos[0] += np.sum(dx)
     pos[1] += np.sum(dy)
     pos[2] += np.sum(dz)
-    return pos
-
+    return pos, dx, dy, dz
+dx = np.empty([])
+dy = np.empty([])
+dz = np.empty([])
 pos = [0.0, 0.0, 0.0]
-pos = step(pos, 500000000, 0.0, 3241363943)
-print(pos[0], ", ", pos[1], ", ", pos[2])
+pos = step(pos, 0.0, 324136.394, 10000000)
 
-plt.hist(pos[0], bins = 'fd', density = True)
-plt.savefig("Xhistogramyear.pdf")
-plt.clf()
-plt.hist(pos[1], bins = 'fd', density = True)
-plt.savefig("Yhistogramyear.pdf")
-plt.clf()
-plt.hist(pos[2], bins = 'fd', density = True)
-plt.savefig("Zhistogramyear.pdf")
-plt.clf()
+x = 0
+y = 0
+z = 0
+r = 0
+i = 0
+while (r < 636.94E6):
+    x += dx[i]
+    y += dy[i]
+    z += dz[i]
+    r = SQRT(x**2 + y**2 + z**2)
+    i += 1
+
+print(pos[0], ", ", pos[1], ", ", pos[2])
+print(i)
 
 #sigman = SQRT(N)*sigma
--- randomWalk2.py
+++ randomWalk2.py
@@ -3,18 +3,21 @@ import math
 import random
 import matplotlib.pyplot as plt
 
-dx = normal(0.0, 3241363943, 10000000)
-dy = normal(0.0, 3241363943, 10000000)
-dz = normal(0.0, 3241363943, 10000000)
-x = 0, y = 0, z = 0, r = 0, i = 0
-while (r < 636.94E6):
+dx = np.random.normal(0.0, 324136.3943, 10000000)
+dy = np.random.normal(0.0, 324136.3943, 10000000)
+dz = np.random.normal(0.0, 324136.3943, 10000000)
+x = 0
+y = 0
+z = 0
+r = 0
+i = 0
+while (r < 695700000):
     x += dx[i]
     y += dy[i]
     z += dz[i]
-    r = SQRT(x**2 + y**2 + z**2)
+    r = np.sqrt(x**2 + y**2 + z**2)
     i += 1
 
-print(i)
-
-#t = i in years
+t = i # in years
+print(t)
 
